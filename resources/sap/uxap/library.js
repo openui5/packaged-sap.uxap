9da2d725cb16df33331caea87fa81f063ffdb02c
OpenUI5 <openui5@sap.com>
2016-02-11 11:16:42 +0100

    1.34.6

--- resources/sap/uxap/library.js
+++ resources/sap/uxap/library.js
@@ -48,7 +48,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Core", "sap/ui/core/library", "
 			"sap.uxap.ModelMapping",
 			"sap.uxap.ObjectPageHeaderLayoutData"
 		],
-		version: "1.34.5"
+		version: "1.34.6"
 	});
 
 	/**
