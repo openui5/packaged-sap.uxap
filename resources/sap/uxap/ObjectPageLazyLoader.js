4b876d2d033ccc779f17e6c3bfc27cc935d7ca44
OpenUI5 <openui5@sap.com>
2016-07-25 12:28:58 +0200

    1.40.3

--- resources/sap/uxap/ObjectPageLazyLoader.js
+++ resources/sap/uxap/ObjectPageLazyLoader.js
@@ -33,7 +33,7 @@ sap.ui.define([
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.40.2
+	 * @version 1.40.3
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/uxap/library.js
+++ resources/sap/uxap/library.js
@@ -48,7 +48,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Core", "sap/ui/core/library", "
 			"sap.uxap.ModelMapping",
 			"sap.uxap.ObjectPageHeaderLayoutData"
 		],
-		version: "1.40.2",
+		version: "1.40.3",
 		extensions: {
 			flChangeHandlers: {
 				"sap.uxap.ObjectPageLayout": {
