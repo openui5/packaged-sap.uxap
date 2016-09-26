afd4b36e24e6895325db96beed64917ff4ebcba8
OpenUI5 <openui5@sap.com>
2016-09-26 19:37:56 +0200

    1.42.2

--- resources/sap/uxap/ObjectPageLayout.js
+++ resources/sap/uxap/ObjectPageLayout.js
@@ -1469,6 +1469,10 @@ sap.ui.define([
 
 			this._adjustLayout(null, true);
 
+			if (this.getFooter() && this.getShowFooter()) {
+				this._shiftFooter();
+			}
+
 			this._scrollTo(this._$opWrapper.scrollTop(), 0);
 		});
 
--- resources/sap/uxap/library.js
+++ resources/sap/uxap/library.js
@@ -48,7 +48,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Core", "sap/ui/core/library", "
 			"sap.uxap.ModelMapping",
 			"sap.uxap.ObjectPageHeaderLayoutData"
 		],
-		version: "1.42.0",
+		version: "1.42.2",
 		extensions: {
 			flChangeHandlers: {
 				"sap.uxap.ObjectPageLayout": {
