82736f98678d1091ea805d9e31fbd76c39a743dd
OpenUI5 <openui5@sap.com>
2017-07-25 17:28:54 +0200

    1.44.18

--- resources/sap/uxap/BlockBaseMetadata.js
+++ resources/sap/uxap/BlockBaseMetadata.js
@@ -17,7 +17,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/ElementMetadata"], function (jQ
 	 *
 	 * @class
 	 * @author SAP SE
-	 * @version 1.44.17
+	 * @version 1.44.18
 	 * @since 1.26
 	 * @alias sap.uxap.BlockBaseMetadata
 	 */
