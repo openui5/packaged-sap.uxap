94de4dafa27813e226fe2fec19af826b88fa4eba
OpenUI5 <openui5@sap.com>
2018-01-15 14:34:44 +0100

    1.52.4

--- resources/sap/uxap/ObjectPageSection.js
+++ resources/sap/uxap/ObjectPageSection.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2017 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2018 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -89,7 +89,7 @@ sap.ui.define([
 	 * @private
 	 */
 	ObjectPageSection._getClosestSection = function (vSectionBase) {
-		var oSectionBase = (typeof vSectionBase === "string") ? sap.ui.getCore().byId(vSectionBase) : vSectionBase;
+		var oSectionBase = (typeof vSectionBase === "string" && sap.ui.getCore().byId(vSectionBase)) || vSectionBase;
 		return (oSectionBase instanceof ObjectPageSubSection) ? oSectionBase.getParent() : oSectionBase;
 	};
 
