841c6ef771e3e326833461f398a1822f62480d05
Matthias Osswald <mat.osswald@sap.com>
2018-03-14 15:02:05 +0100

    1.54.1

--- resources/sap/uxap/BlockBaseMetadata.js
+++ resources/sap/uxap/BlockBaseMetadata.js
@@ -17,7 +17,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/ElementMetadata"], function (jQ
 	 *
 	 * @class
 	 * @author SAP SE
-	 * @version 1.54.0
+	 * @version 1.54.1
 	 * @since 1.26
 	 * @alias sap.uxap.BlockBaseMetadata
 	 */
--- resources/sap/uxap/changeHandler/RenameObjectPageSection.js
+++ resources/sap/uxap/changeHandler/RenameObjectPageSection.js
@@ -18,7 +18,7 @@
 			 * @constructor
 			 * @alias sap.uxap.changeHandler.RenameObjectPageSection
 			 * @author SAP SE
-			 * @version 1.54.0
+			 * @version 1.54.1
 			 * @experimental Since 1.50
 			 */
 
