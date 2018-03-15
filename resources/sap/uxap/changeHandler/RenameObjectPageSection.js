84bbc4bdb9112083c1f2fdd41fe8f0031fbd8caf
OpenUI5 <openui5@sap.com>
2018-03-15 18:51:36 +0100

    1.52.9

--- resources/sap/uxap/changeHandler/RenameObjectPageSection.js
+++ resources/sap/uxap/changeHandler/RenameObjectPageSection.js
@@ -18,7 +18,7 @@
 			 * @constructor
 			 * @alias sap.uxap.changeHandler.RenameObjectPageSection
 			 * @author SAP SE
-			 * @version 1.52.8
+			 * @version 1.52.9
 			 * @experimental Since 1.50
 			 */
 
--- resources/sap/uxap/library.js
+++ resources/sap/uxap/library.js
@@ -53,7 +53,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Core", "sap/ui/base/DataType", 
 			"sap.uxap.ModelMapping",
 			"sap.uxap.ObjectPageHeaderLayoutData"
 		],
-		version: "1.52.8",
+		version: "1.52.9",
 		extensions: {
 			flChangeHandlers: {
 				"sap.uxap.ObjectPageHeader" : "sap/uxap/flexibility/ObjectPageHeader",
