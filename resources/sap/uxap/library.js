afe4fd7c4bea9885475a563b8ce9826205b17891
OpenUI5 <openui5@sap.com>
2018-05-28 18:30:08 +0200

    1.44.32

--- resources/sap/uxap/library.js
+++ resources/sap/uxap/library.js
@@ -48,7 +48,7 @@ sap.ui.define(["jquery.sap.global", "sap/ui/core/Core", "sap/ui/core/library", "
 			"sap.uxap.ModelMapping",
 			"sap.uxap.ObjectPageHeaderLayoutData"
 		],
-		version: "1.44.31",
+		version: "1.44.32",
 		extensions: {
 			flChangeHandlers: {
 				"sap.uxap.ObjectPageLayout": {
