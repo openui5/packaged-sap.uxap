6c84de9ffd342238bc6f9e41464f062caf6dd4da
OpenUI5 <openui5@sap.com>
2019-07-30 17:37:53 +0200

    1.52.31

--- resources/sap/uxap/ObjectPageDynamicHeaderTitle.js
+++ resources/sap/uxap/ObjectPageDynamicHeaderTitle.js
@@ -30,7 +30,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/uxap/ObjectPageDynamicHead
 		 * @implements sap.uxap.IHeaderTitle
 		 *
 		 * @author SAP SE
-		 * @version 1.52.30
+		 * @version 1.52.31
 		 *
 		 * @constructor
 		 * @public
