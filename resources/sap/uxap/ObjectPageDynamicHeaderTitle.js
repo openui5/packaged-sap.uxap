b42defc15092fa093e003353d3590e5fb4acce1a
OpenUI5 <openui5@sap.com>
2020-04-21 16:40:26 +0200

    1.52.40

--- resources/sap/uxap/ObjectPageDynamicHeaderTitle.js
+++ resources/sap/uxap/ObjectPageDynamicHeaderTitle.js
@@ -30,7 +30,7 @@ sap.ui.define(['jquery.sap.global', './library', 'sap/uxap/ObjectPageDynamicHead
 		 * @implements sap.uxap.IHeaderTitle
 		 *
 		 * @author SAP SE
-		 * @version 1.52.39
+		 * @version 1.52.40
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/uxap/changeHandler/RenameObjectPageSection.js
+++ resources/sap/uxap/changeHandler/RenameObjectPageSection.js
@@ -18,7 +18,7 @@
 			 * @constructor
 			 * @alias sap.uxap.changeHandler.RenameObjectPageSection
 			 * @author SAP SE
-			 * @version 1.52.39
+			 * @version 1.52.40
 			 * @experimental Since 1.50
 			 */
 
